4a782f83851815ceb476711b88c8ea82657f8f45
obayedullah <obayedullahkhan986@gmail.com>
2024-03-25 14:11:39 +0600

    project updated

--- src/Pages/Home/Header/Header.js
+++ src/Pages/Home/Header/Header.js
@@ -11,6 +11,18 @@ const Header = () => {
     return (
         <section class="section hero" aria-label="hero">
             <div class="container">
+
+            <figure class="hero-banner">
+                    <span><Lottie animationData={transparent}></Lottie> </span>
+
+                    <img src={heroIcon1} alt="you tube" width="100" height="100"
+                        class="shape shape-1" data-parallax data-parallax-speed="15" />
+                    <img src={heroIcon2} alt="twitter" width="120" height="120"
+                        class="shape shape-2" data-parallax data-parallax-speed="25" />
+                    <img src={heroIcon3} alt="instagram" width="150" height="150"
+                        class="shape shape-3" data-parallax data-parallax-speed="40" />
+                </figure>
+
                 <div class="hero-content">
                     <h1 class="h1 title hero-title">
                         We take care of your brand
@@ -26,18 +38,8 @@ const Header = () => {
                         <input type="email" name="email-address" class="input-field" placeholder="Enter your email" />
                         <button type="submit" class="btn btn-primary">Let's Talk</button>
                     </div>
-                </div>
-
-                <figure class="hero-banner">
-                    <span><Lottie animationData={transparent}></Lottie> </span>
-
-                    <img src={heroIcon1} alt="you tube" width="100" height="100"
-                        class="shape shape-1" data-parallax data-parallax-speed="15" />
-                    <img src={heroIcon2} alt="twitter" width="120" height="120"
-                        class="shape shape-2" data-parallax data-parallax-speed="25" />
-                    <img src={heroIcon3} alt="instagram" width="150" height="150"
-                        class="shape shape-3" data-parallax data-parallax-speed="40" />
-                </figure>
+                </div> 
+                
             </div>
         </section>
     );
